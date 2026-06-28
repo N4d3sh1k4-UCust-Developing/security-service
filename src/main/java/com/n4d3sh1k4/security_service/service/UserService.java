43576113e8807ff218b8d55dd25473271107055d
N4d3sh <vusezet@gmail.com>
2026-06-29 02:43:01 +0300

    fix: code optimization

--- src/main/java/com/n4d3sh1k4/security_service/service/UserService.java
+++ src/main/java/com/n4d3sh1k4/security_service/service/UserService.java
@@ -55,7 +55,6 @@ public class UserService {
                             finalLastName,
                             phone
                     ));
-
                     return newUser;
                 });
     }
