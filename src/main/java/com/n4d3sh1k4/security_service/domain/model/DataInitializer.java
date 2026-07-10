531be23c03129773b63d4445a7d63c3c69e03504
N4d3sh <vusezet@gmail.com>
2026-07-10 17:12:42 +0300

    feat: update initialise data logic

--- src/main/java/com/n4d3sh1k4/security_service/domain/model/DataInitializer.java
+++ src/main/java/com/n4d3sh1k4/security_service/domain/model/DataInitializer.java
@@ -1,12 +1,17 @@
 package com.n4d3sh1k4.security_service.domain.model;
 
+import com.n4d3sh1k4.security_service.domain.model.users.AuthProvider;
 import com.n4d3sh1k4.security_service.domain.model.users.Privilege;
 import com.n4d3sh1k4.security_service.domain.model.users.Role;
+import com.n4d3sh1k4.security_service.domain.model.users.User;
 import com.n4d3sh1k4.security_service.domain.repository.PrivilegeRepository;
 import com.n4d3sh1k4.security_service.domain.repository.RoleRepository;
+import com.n4d3sh1k4.security_service.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -17,6 +22,14 @@ public class DataInitializer implements CommandLineRunner {
 
     private final RoleRepository roleRepository;
     private final PrivilegeRepository privilegeRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder; // Из Spring Security для хэширования
+
+    @Value("${app.default-admin.email}")
+    private String adminEmail;
+
+    @Value("${app.default-admin.password}")
+    private String adminPassword;
 
     @Override
     public void run(String @NonNull ... args) {
@@ -32,18 +45,17 @@ public class DataInitializer implements CommandLineRunner {
 
             Role adminRole = new Role("ADMIN");
             adminRole.setPrivileges(List.of(read, write));
+            adminRole = roleRepository.save(adminRole);
 
-//            User admin = new User();
-//            admin.setUsername(adminUsername);
-//            admin.setEmail(adminEmail);
-//            admin.setPasswordHash(passwordEncoder.encode(adminPassword));
-//            admin.setProvider(AuthProvider.LOCAL);
-//            admin.setEnabled(true);
-//            admin.setAccountNonLocked(true);
-//            admin.setRoles(List.of(adminRole));
+            User admin = new User();
+            admin.setEmail(adminEmail);
+            admin.setPasswordHash(passwordEncoder.encode(adminPassword));
+            admin.setProvider(AuthProvider.LOCAL);
+            admin.setEnabled(true);
+            admin.setAccountNonLocked(true);
+            admin.setRoles(List.of(adminRole));
 
-            roleRepository.save(adminRole);
+            userRepository.save(admin);
         }
     }
 }
-
